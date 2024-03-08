7cd0c7611683180f49a9eebd80f51ba3d91ca5bf
Gaurav <gauravkrdas19@gmail.com>
2024-03-08 14:57:20 +0530

    Selcetion UI

--- app_stuffs/lib/properties/global_colors.dart
+++ app_stuffs/lib/properties/global_colors.dart
@@ -8,4 +8,6 @@ class AppColor {
   static const Color tomato = Color(0xFFff6347);
   static const Color forestGreen = Color(0xFF4E8B25);
   static const Color white = Color(0xffffffff);
+  static const Color right = Color(0x314E8B25);
+  static const Color wrong = Color(0x31ff6347);
 }
--- app_stuffs/lib/screens/quizes_screen.dart
+++ app_stuffs/lib/screens/quizes_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gcu_knowledge_hub/widgets/options.dart';
 import 'package:gcu_knowledge_hub/widgets/prev_next_btns.dart';
 import 'package:gcu_knowledge_hub/widgets/question_no_display.dart';
 import 'package:gcu_knowledge_hub/widgets/quiz_info.dart';
@@ -14,31 +13,32 @@ class QuizesScreen extends StatefulWidget {
   final bool isUnderDevelopment;
   final String subName;
   final Map<String, dynamic> quizQuestions;
-  const QuizesScreen(
-      {super.key,
-      required this.subName,
-      required this.quizQuestions,
-      required this.isUnderDevelopment});
+  const QuizesScreen({
+    Key? key,
+    required this.subName,
+    required this.quizQuestions,
+    required this.isUnderDevelopment,
+  }) : super(key: key);
 
   @override
   State<QuizesScreen> createState() => _QuizesScreenState();
 }
 
 class _QuizesScreenState extends State<QuizesScreen> {
-  int answered = 0;
-  int index = 0;
   late bool isUnderDevelopment;
-  late int unanswered;
   late String subName;
   late Map<String, dynamic> quizQuestions;
+  List<String> selectedOptions = []; // Track selected options for each question
+  int index = 0;
 
   @override
   void initState() {
     super.initState();
-    unanswered = widget.quizQuestions.length;
     subName = widget.subName;
     quizQuestions = widget.quizQuestions;
     isUnderDevelopment = widget.isUnderDevelopment;
+    // Initialize the list with empty strings for each question
+    selectedOptions = List.filled(widget.quizQuestions.length, '');
   }
 
   @override
@@ -81,8 +81,6 @@ class _QuizesScreenState extends State<QuizesScreen> {
                     children: [
                       QuizInfoBar(
                         totalQuestions: quizQuestions.length,
-                        unanswered: unanswered,
-                        answered: answered,
                       ),
                       const SizedBox(
                         height: 20,
@@ -107,48 +105,32 @@ class _QuizesScreenState extends State<QuizesScreen> {
                                 const SizedBox(
                                   height: 20,
                                 ),
-                                MCQOptions(
-                                  optionNo: quizQuestions.values
-                                      .elementAt(index)['choice']
-                                      .keys
-                                      .elementAt(0)
-                                      .toString(),
+                                buildOptionWidget(
+                                  optionNo: 'A',
                                   choiceDesc: quizQuestions.values
                                       .elementAt(index)['choice']
                                       .values
                                       .elementAt(0)
                                       .toString(),
                                 ),
-                                MCQOptions(
-                                  optionNo: quizQuestions.values
-                                      .elementAt(index)['choice']
-                                      .keys
-                                      .elementAt(1)
-                                      .toString(),
+                                buildOptionWidget(
+                                  optionNo: 'B',
                                   choiceDesc: quizQuestions.values
                                       .elementAt(index)['choice']
                                       .values
                                       .elementAt(1)
                                       .toString(),
                                 ),
-                                MCQOptions(
-                                  optionNo: quizQuestions.values
-                                      .elementAt(index)['choice']
-                                      .keys
-                                      .elementAt(2)
-                                      .toString(),
+                                buildOptionWidget(
+                                  optionNo: 'C',
                                   choiceDesc: quizQuestions.values
                                       .elementAt(index)['choice']
                                       .values
                                       .elementAt(2)
                                       .toString(),
                                 ),
-                                MCQOptions(
-                                  optionNo: quizQuestions.values
-                                      .elementAt(index)['choice']
-                                      .keys
-                                      .elementAt(3)
-                                      .toString(),
+                                buildOptionWidget(
+                                  optionNo: 'D',
                                   choiceDesc: quizQuestions.values
                                       .elementAt(index)['choice']
                                       .values
@@ -181,6 +163,82 @@ class _QuizesScreenState extends State<QuizesScreen> {
     );
   }
 
+  // Function to create the option widget
+  Widget buildOptionWidget(
+      {required String optionNo, required String choiceDesc}) {
+    return GestureDetector(
+      onTap: () => optionSelected(optionNo),
+      child: Container(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height * 0.07,
+        ),
+        margin: const EdgeInsets.only(
+          bottom: 10,
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        decoration: BoxDecoration(
+          color: (selectedOptions[index] == optionNo &&
+                  quizQuestions.values.elementAt(index)['ans'] == optionNo)
+              ? AppColor.right
+              : (selectedOptions[index] == optionNo &&
+                      quizQuestions.values.elementAt(index)['ans'] != optionNo)
+                  ? AppColor.wrong
+                  : null,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          border: Border.fromBorderSide(
+            BorderSide(
+              width: (selectedOptions[index] == optionNo) ? 2 : 1,
+              color: (selectedOptions[index] == optionNo &&
+                      quizQuestions.values.elementAt(index)['ans'] == optionNo)
+                  ? AppColor.forestGreen
+                  : (selectedOptions[index] == optionNo &&
+                          quizQuestions.values.elementAt(index)['ans'] !=
+                              optionNo)
+                      ? AppColor.tomato
+                      : AppColor.grey,
+            ),
+          ),
+        ),
+        child: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                optionNo,
+                style: const TextStyle(
+                  color: AppColor.marianBlue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Flexible(
+                child: Text(
+                  choiceDesc,
+                  overflow: TextOverflow.clip,
+                  style: const TextStyle(
+                    color: AppColor.marianBlue,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Function to handle option selection
+  void optionSelected(String optionNo) {
+    setState(() {
+      selectedOptions[index] = optionNo;
+    });
+  }
+
+  // Function to move to the next question
   void nextQuestion() {
     if (index < quizQuestions.length - 1) {
       setState(() {
@@ -189,6 +247,7 @@ class _QuizesScreenState extends State<QuizesScreen> {
     }
   }
 
+  // Function to move to the previous question
   void prevQuestion() {
     if (index > 0) {
       setState(() {
