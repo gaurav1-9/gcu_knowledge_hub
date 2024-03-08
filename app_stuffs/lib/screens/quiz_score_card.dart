953c8b6ef5a49b7a3a384803ed52e5dd653bb5f3
Gaurav <gauravkrdas19@gmail.com>
2024-03-08 17:14:13 +0530

    get unanswered,wrongAns and correctAnswer

--- app_stuffs/lib/screens/quiz_score_card.dart
+++ app_stuffs/lib/screens/quiz_score_card.dart
@@ -4,16 +4,42 @@ import '../properties/global_colors.dart';
 import '../widgets/buttons/app_bar_back_btn.dart';
 import '../widgets/gcu.dart';
 
-class QuizScoreCard extends StatelessWidget {
+class QuizScoreCard extends StatefulWidget {
   final int totalQuestions;
   final List selectedQuestions;
   final List correctAnswers;
+
   const QuizScoreCard({
     super.key,
     required this.totalQuestions,
     required this.selectedQuestions,
     required this.correctAnswers,
   });
+
+  @override
+  State<QuizScoreCard> createState() => _QuizScoreCardState();
+}
+
+class _QuizScoreCardState extends State<QuizScoreCard> {
+  late int correctlyAnswered = 0;
+  late int wrongAns = 0;
+  late int unanswered = 0;
+  late int totalQuestions;
+
+  @override
+  void initState() {
+    super.initState();
+    totalQuestions = widget.totalQuestions;
+    for (var i = 0; i < totalQuestions; i++) {
+      if (widget.selectedQuestions[i] == "") {
+        unanswered++;
+      } else if (widget.selectedQuestions[i] == widget.correctAnswers[i]) {
+        correctlyAnswered++;
+      } else {
+        wrongAns++;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +54,17 @@ class QuizScoreCard extends StatelessWidget {
           alignment: MainAxisAlignment.start,
         ),
         backgroundColor: AppColor.jonquil,
+      ),
+      body: Column(
+        children: [
+          const Text(
+            "Well Done!!",
+          ),
+          Text("Total questions: ${totalQuestions.toString()}"),
+          Text("Unanswered: $unanswered"),
+          Text("Correct: $correctlyAnswered"),
+          Text("wrong: $wrongAns"),
+        ],
       ),
     );
   }
