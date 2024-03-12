1596bad378899b35622f41396124785a34ca307b
Gaurav <gauravkrdas19@gmail.com>
2024-03-12 12:04:45 +0530

    basic layout (admin dept-details, quiz-details & subject-details)

--- app_stuffs/lib/screens/admin_dept_details.dart
+++ app_stuffs/lib/screens/admin_dept_details.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../properties/global_colors.dart';
+import '../widgets/buttons/app_bar_back_btn.dart';
+import '../widgets/gcu.dart';
+
 class AdminDeptDetails extends StatefulWidget {
   const AdminDeptDetails({super.key});
 
@@ -10,6 +14,19 @@ class AdminDeptDetails extends StatefulWidget {
 class _AdminDeptDetailsState extends State<AdminDeptDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        titleSpacing: 1,
+        leading: const AppBarBackBtn(),
+        title: const GCUTextLogo(
+          size: 60,
+          fontSize: 16,
+          alignment: MainAxisAlignment.start,
+        ),
+        backgroundColor: AppColor.jonquil,
+      ),
+      body: Text("Department Details"),
+    );
   }
 }
--- app_stuffs/lib/screens/admin_quiz_details.dart
+++ app_stuffs/lib/screens/admin_quiz_details.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../properties/global_colors.dart';
+import '../widgets/buttons/app_bar_back_btn.dart';
+import '../widgets/gcu.dart';
+
 class AdminQuizDetails extends StatefulWidget {
   const AdminQuizDetails({super.key});
 
@@ -10,6 +14,19 @@ class AdminQuizDetails extends StatefulWidget {
 class _AdminQuizDetailsState extends State<AdminQuizDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        titleSpacing: 1,
+        leading: const AppBarBackBtn(),
+        title: const GCUTextLogo(
+          size: 60,
+          fontSize: 16,
+          alignment: MainAxisAlignment.start,
+        ),
+        backgroundColor: AppColor.jonquil,
+      ),
+      body: Text("Quiz Details"),
+    );
   }
 }
